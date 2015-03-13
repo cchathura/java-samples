4eb06059a28b386901023c43e84ee04f838e3de6
Chathura Prasad Amarathunga <cchathura@ymail.com>
2015-03-13 23:02:06 +0530

    add method to cmenucomponent

--- src/com/chathura/iterator/menu/MenuComponent.java
+++ src/com/chathura/iterator/menu/MenuComponent.java
@@ -1,7 +1,10 @@
 package com.chathura.iterator.menu;
 
+import java.util.Iterator;
+
 public interface MenuComponent {
 public void add(MenuComponent menu);
 public MenuComponent getMenu();
 public void remove (MenuComponent menu);
+public Iterator createIterator();
 }
